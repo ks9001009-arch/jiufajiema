64a9b01f233842ff48e6abaf524fc60d298cc28e
KK <vuquy4505@gmail.com>
2026-07-08 14:52:34 -0400

    feat(web): add login flow

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,11 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
